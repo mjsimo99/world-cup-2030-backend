56745ce69ac26cdd197049a2476f253e3d7fb1fd
mjsimo99 <mohashark21@gmail.com>
2024-03-26 11:18:07 +0000

    create entity city

---
+++ src/main/java/com/example/satocup/model/entity/City.java
@@ -0,0 +1,31 @@
+package com.example.satocup.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Cities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cityId;
+
+    @Column(name = "name")
+    @NotBlank(message = "City name is required")
+    @Size(max = 50, message = "City name must be less than or equal to 50 characters")
+    private String name;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Stadium> stadiums;
+}
