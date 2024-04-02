c64fc3d32a4494f2535982f1b751b0765fa1ad44
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:34:39 +0000

    more validations

--- src/main/java/com/example/satocup/model/dto/TicketDTO.java
+++ src/main/java/com/example/satocup/model/dto/TicketDTO.java
@@ -1,5 +1,6 @@
 package com.example.satocup.model.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -10,15 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TicketDTO {
-    @NotNull(message = "Ticket ID cannot be null")
     private Long ticketId;
 
     @Min(value = 1, message = "Quantity must be at least 1")
+    @Max(value = 4, message = "Quantity must be at most 4")
     private int quantity;
 
-    @NotNull(message = "Client ID cannot be null")
     private Long clientId;
 
-    @NotNull(message = "Match ID cannot be null")
     private Long matchId;
+
+    @Min(value = 0, message = "Total price cannot be negative")
+    private double totalPrice;
+
 }
