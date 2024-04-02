b0cde611fd082db56417222020a3aabee7b5cba4
mjsimo99 <mohashark21@gmail.com>
2024-04-02 11:36:02 +0000

    increase size token

--- src/main/java/com/example/satocup/model/security/Token.java
+++ src/main/java/com/example/satocup/model/security/Token.java
@@ -19,7 +19,7 @@ public class Token {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "token",length = 500)
+    @Column(name = "token",length = 1000)
     private String token;
 
     @Column(name = "is_logged_out")
